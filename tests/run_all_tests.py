96c600395ddbe0a28afa86dc5aeaced9df70f446
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2022-01-19 15:25:01 -0500

    Test directory path is absolute.

--- tests/run_all_tests.py
+++ tests/run_all_tests.py
@@ -2,6 +2,6 @@ from os import system
 from pathlib import Path
 
 
-local_dir = Path(__file__).parent
+local_dir = Path(__file__).parent.resolve()
 
 system(f"pytest {local_dir/'field_type_tests.py'}")
