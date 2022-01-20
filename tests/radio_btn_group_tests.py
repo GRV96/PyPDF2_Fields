774652bb9ba774312e0fd7a763f82ee3c528c842
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2022-01-20 11:56:48 -0500

    getitem tests

--- tests/radio_btn_group_tests.py
+++ tests/radio_btn_group_tests.py
@@ -3,14 +3,47 @@ import pytest
 from ..src import RadioBtnGroup
 
 
+def getitem_exception_test(index):
+	button_group = make_radio_button_group()
+	except_msg = f"Radio button group LeGroupe does not have index {index}."
+	with pytest.raises(IndexError, match=except_msg):
+		button_group[index]
+
+
 def make_radio_button_group():
 	return RadioBtnGroup("LeGroupe", "BtnA", "BtnB", "BtnC")
 
 
-def test_init_except():
+def test_init_exception():
 	except_msg = "At least one button name must be provided."
 	with pytest.raises(ValueError, match=except_msg):
 		button_group = RadioBtnGroup("LeGroupe")
+
+
+def test_getitem_a():
+	button_group = make_radio_button_group()
+	assert button_group[0] == "BtnA"
+	assert button_group[-3] == "BtnA"
+
+
+def test_getitem_b():
+	button_group = make_radio_button_group()
+	assert button_group[1] == "BtnB"
+	assert button_group[-2] == "BtnB"
+
+
+def test_getitem_c():
+	button_group = make_radio_button_group()
+	assert button_group[2] == "BtnC"
+	assert button_group[-1] == "BtnC"
+
+
+def test_getitem_exception_positive_index():
+	getitem_exception_test(3)
+
+
+def test_getitem_exception_negative_index():
+	getitem_exception_test(-4)
 
 
 def test_name():
