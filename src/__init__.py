32704f2888e2734aded77e0d32c7ea9928b384ce
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2022-01-21 12:31:18 -0500

    Content from module pypdf2_fields exported

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,12 @@
+import imp
 from .field_types import\
 	PdfFieldType,\
 	get_field_type
+
+from .pypdf2_fields import\
+	make_writer_from_reader,\
+	pdf_field_name_val_dict,\
+	set_need_appearances,\
+	update_page_fields
 
 from .radio_btn_group import RadioBtnGroup
