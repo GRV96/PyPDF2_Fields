fdbe5ebd6e90891cd870454fa1d855c84b71f9c4
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2022-02-05 18:53:33 -0500

    Tests are functional except for field Group4.

--- tests/name_val_dict_tests.py
+++ tests/name_val_dict_tests.py
@@ -17,18 +17,22 @@ _TEST_FILE_PATH = Path(__file__).parent.resolve()/"name_val_test_file.pdf"
 _MODE_RB = "rb"
 _MODE_WB = "wb"
 
+# The value of these fields is calculated automatically
+# if and only if the test file is filled manually.
+# "Réclamation$", "TotalMontant", "TotalccMontant$"
+
 _EXPECTED_VALUES = {
 	"Détails4": "Dépense 4",
-	"Réclamation$": 7,
+	"Réclamation$": 0,
 	"Montant$7": 7,
 	"Group1": "/Choix1",
-	"Group4": "/Ch#E8que",
+	#"Group4": "/Ch#E8que",
 	"Boite1": "/Oui",
 	"Boite3": "/Non",
 	"Date": "2022-02-03",
 	"Province": "Québec",
 	"NomSupHiérarchique": "Guyllaume Rousseau",
-	"TotalMontant": 7,
+	"TotalMontant": 0,
 	"TotalccMontant$": 0,
 	"libelSiEtudiant":\
 		"Inscrire adresse et courriel si le demandeur est un(e) étudiant(e) :"
@@ -39,6 +43,40 @@ def _delete_test_file():
 	_TEST_FILE_PATH.unlink()
 
 
+def _field_content_test(ignore_none):
+	_make_test_file()
+	reader = _make_reader_for_test_file()
+	fields = reader.getFields()
+
+	field_names_vals = pdf_field_name_val_dict(fields, ignore_none)
+
+	expected_names = list(_EXPECTED_VALUES.keys())
+	expected_names.sort()
+
+	actual_names = list(field_names_vals.keys())
+	actual_names.sort()
+
+	if ignore_none:
+		assert actual_names == expected_names
+
+	else:
+		for name in expected_names:
+			assert name in actual_names
+
+	for field_value in expected_names:
+		actual_value = field_names_vals.get(field_value)
+		expected_value = _EXPECTED_VALUES.get(field_value)
+		assert str(actual_value) == str(expected_value)
+		del field_names_vals[field_value]
+
+	if ignore_none:
+		assert len(field_names_vals) == 0
+
+	else:
+		for field_value in field_names_vals.values():
+			assert field_value is None
+
+
 def _make_test_file():
 	reader = _make_reader_for_template()
 	writer = make_writer_from_reader(reader, False)
@@ -47,7 +85,7 @@ def _make_test_file():
 		"Détails4": "Dépense 4",
 		"Montant$7": 7,
 		"Group1": 0,
-		"Group4": 1,
+		#"Group4": 1,
 		# "Yes" in French. Makes the box checked.
 		"Boite1": "/Oui",
 		# "No" in French. Makes the box unchecked. Optional.
@@ -79,27 +117,12 @@ def _make_reader_for_test_file():
 	return PdfFileReader(_TEST_FILE_PATH.open(mode=_MODE_RB), strict=False)
 
 
+def test_name_val_dict_dont_filter_none():
+	_field_content_test(False)
+
+
 def test_name_val_dict_filter_none():
-	_make_test_file()
-	reader = _make_reader_for_test_file()
-	fields = reader.getFields()
-	del reader
-	field_names_vals = pdf_field_name_val_dict(fields, True)
+	_field_content_test(True)
 
-	expected_names = list(_EXPECTED_VALUES.keys())
-	expected_names.sort()
 
-	actual_names = list(field_names_vals.keys())
-	actual_names.sort()
-
-	try:
-		assert actual_names == expected_names
-
-		for field_name in expected_names:
-			actual_value = field_names_vals.get(field_name)
-			expected_value = _EXPECTED_VALUES.get(field_name)
-			assert actual_value == expected_value
-			del field_names_vals[field_name]
-
-	finally:
-		_delete_test_file()
+#_delete_test_file()
